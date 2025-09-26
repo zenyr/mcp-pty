cc888b263a7bf4c7f78903c7499f3fa5ffe4334d
Jinhyeok Lee <zenyr@zenyr.com>
2025-09-26 12:06:36 +0900

    feat: add initial setup for pty-manager package
    pty-manager 패키지의 기본 설정 파일들을 추가하여 개발 환경을 구성합니다.

---
+++ packages/pty-manager/eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-console": "off",
+    },
+  },
+];
